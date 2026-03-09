d11af2eb7040e9771e25831f9e5d27837d413582
Kinza <kinza@example.com>
2026-03-09 16:57:56 +0500

    Update server port to 8081

--- start-server.ps1
+++ start-server.ps1
@@ -1,7 +1,7 @@
 # EQUITY Finance App – PowerShell HTTP Server
 # Run: powershell -ExecutionPolicy Bypass -File start-server.ps1
 
-$port = 8080
+$port = 8081
 $dir  = $PSScriptRoot
 
 $listener = [System.Net.HttpListener]::new()
